c1ce5dde7a7991e2b4c8cdf9bb0018709748d3fe
Aya Hosny <ayazaine@gmail.com>
2022-07-27 16:31:43 +0200

    translation

--- database/migrations/2022_07_27_141552_add_sku_in_products_table.php
+++ database/migrations/2022_07_27_141552_add_sku_in_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSkuInProductsTable extends Migration
+class AddTransOfInProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddSkuInProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('SKU');
+            $table->string('translation_of');
         });
     }
 
@@ -26,7 +26,7 @@ class AddSkuInProductsTable extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('SKU');
+            $table->dropColumn('translation_of');
         });
     }
 }
