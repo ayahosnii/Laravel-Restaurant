9d23631337d4eaa0ffc213e055495ada43bd6acc
Aya Hosny <ayazaine@gmail.com>
2022-07-27 16:23:22 +0200

    SKU column

--- database/migrations/2022_07_27_141552_add_sku_in_products_table.php
+++ database/migrations/2022_07_27_141552_add_sku_in_products_table.php
@@ -13,7 +13,7 @@ class AddSkuInProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->string('SKU');
         });
     }
@@ -25,7 +25,7 @@ class AddSkuInProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('SKU');
         });
     }
