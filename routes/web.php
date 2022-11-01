42aba262d6c1a61db92056dddc1899f4798d471d
Aya Hosny <ayazaine@gmail.com>
2022-11-01 08:09:41 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,6 @@ Route::group(['prefix' => LaravelLocalization::setLocale(),
     Route::get('/', \App\Http\Livewire\HomeComponent::class)->name('index');
     Route::get('/shop/main-category/{category_slug}', \App\Http\Livewire\CategoryComponent::class)->name('product.category');
     Route::get('/shop/sub-category/{sub_category_slug}', [\App\Http\Controllers\SubCategoryController::class, 'index'])->name('sub-category.index');
-    Route::get('{any}', function ($any){
-        return view('site.home');
-    })->where('any','.*');
     Route::get('/base', [\App\Http\Controllers\BaseController::class, 'index'])->name('base');
     //Route::get('/shop', \App\Http\Livewire\ShopComponent::class)->name('shop');
     Route::get('/shop', [\App\Http\Controllers\ShopController::class, 'index'])->name('shop');
@@ -46,7 +43,15 @@ Route::group(['prefix' => LaravelLocalization::setLocale(),
     route::get('shop/{slug}', [\App\Http\Controllers\ProductController::class, 'productsBySlug'])->name('product.details');
     route::get('shop/meal/{name}', [\App\Http\Controllers\ProductController::class, 'mealsBySlug'])->name('meal.details');
     route::get('/findprice', [\App\Http\Controllers\ProductController::class, 'findprice'])->name('findprice');
-
+    Route::group(['prefix' => 'cart'], function () {
+        Route::get('/', [\App\Http\Controllers\Site\CartController::class, 'getIndex'])->name('site.cart.index');
+        Route::post('/add/{slug?}', [\App\Http\Controllers\Site\CartController::class, 'postAdd'])->name('site.cart.add');
+        Route::post('/update/{slug}', [\App\Http\Controllers\Site\CartController::class, 'postUpdate'])->name('site.cart.update');
+        Route::post('/update-all', [\App\Http\Controllers\Site\CartController::class, 'postUpdateAll'])->name('site.cart.update-all');
+    });
+    Route::get('{any}', function ($any){
+        return view('site.home');
+    })->where('any','.*');
 });
 
 Route::group(
@@ -78,12 +83,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('wishlist/products', [\App\Http\Controllers\WishlistController::class, 'index'])->name('wishlist.products.index');
 });
 
-Route::group(['prefix' => 'cart'], function () {
-    Route::get('/', [\App\Http\Controllers\Site\CartController::class, 'getIndex'])->name('site.cart.index');
-    Route::post('/add/{slug?}', [\App\Http\Controllers\Site\CartController::class, 'postAdd'])->name('site.cart.add');
-    Route::post('/update/{slug}', [\App\Http\Controllers\Site\CartController::class, 'postUpdate'])->name('site.cart.update');
-    Route::post('/update-all', [\App\Http\Controllers\Site\CartController::class, 'postUpdateAll'])->name('site.cart.update-all');
-});
+
 
 Route::group(['prefix' => 'restaurant'], function () {
     Route::get('/', [\App\Http\Controllers\RestaurantController::class, 'index'])->name('restaurant.index');
