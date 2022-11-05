37bb1b600728034ec7152c188ed9a231f8700fed
Aya Hosny <ayazaine@gmail.com>
2022-11-05 11:03:27 +0200

    chat v2

--- database/seeders/MessagesTableSeeder.php
+++ database/seeders/MessagesTableSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Conversation;
+use App\Models\Message;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class MessagesTableSeeder extends Seeder
@@ -13,6 +16,17 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+        for ($s = 1; $s <= 5; $s++){
+          $conversation = Conversation::whereId($s)->with('users')->first();
+            for ($i = 1; $i <= 5; $i++) {
+                Message::create([
+                    'conversation_id' => $conversation->id,
+                    'user_id' => rand(5, 15),
+                    'body' => $faker->sentence,
+                ]);
+            }
+        }
+
     }
 }
