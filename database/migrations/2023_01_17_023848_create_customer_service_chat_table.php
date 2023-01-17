1b26353758bc0df46182595a5fb2c5dbb8c8f437
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-01-17 11:29:59 +0200

    customer service

--- database/migrations/2023_01_17_023848_create_customer_service_chat_table.php
+++ database/migrations/2023_01_17_023848_create_customer_service_chat_table.php
@@ -15,11 +15,13 @@ class CreateCustomerServiceChatTable extends Migration
     {
        Schema::create('customer_service_chats', function (Blueprint $table) {
            $table->id();
-           $table->uuid('uuid')->constrained()->caseCadeOnDelete();
+           $table->uuid('uuid')->unique()->constrained()->caseCadeOnDelete();
            $table->bigInteger('user_id')->unsigned();
+           $table->bigInteger('admin_id')->unsigned();
            $table->timestamp('last_message_at')->nullable();
            $table->timestamps();
-           $table->foreign('user_id')->references('id')->on('users');
+           $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+           $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade');
        });
     }
 
