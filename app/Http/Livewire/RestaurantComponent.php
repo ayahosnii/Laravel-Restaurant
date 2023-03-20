9a4ed8eb6a800e612b48c1e09dd38badd2b6e095
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-03-20 06:46:57 +0200

    restaurant

--- app/Http/Livewire/RestaurantComponent.php
+++ app/Http/Livewire/RestaurantComponent.php
@@ -113,33 +113,33 @@ class RestaurantComponent extends Component
 
         if($this->sorting=='date')
         {
-            $mealsQuery->whereBetween('id',[$this->min_date,$this->max_date])->orderBy('id', 'ASC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('id',[$this->min_date,$this->max_date])->orderBy('id', 'ASC');
         }
         else if ($this->sorting=="date-desc")
         {
-            $mealsQuery->whereBetween('id',[$this->min_date,$this->max_date])->orderBy('id', 'DESC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('id',[$this->min_date,$this->max_date])->orderBy('id', 'DESC');
         }
         else if ($this->sorting=="price")
         {
-            $mealsQuery->whereBetween('price',[$this->min_price,$this->max_price])->orderBy('price', 'ASC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('price',[$this->min_price,$this->max_price])->orderBy('price', 'ASC');
 
         }
         else if ($this->sorting=="price-desc")
         {
-            $mealsQuery->whereBetween('price',[$this->min_price,$this->max_price])->orderBy('price', 'DESC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('price',[$this->min_price,$this->max_price])->orderBy('price', 'DESC');
         }
         else if ($this->sorting=="alphabet")
         {
-            $mealsQuery->whereBetween('name', [$this->min_alphabet,$this->max_alphabet])->orderBy('name', 'ASC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('name', [$this->min_alphabet,$this->max_alphabet])->orderBy('name', 'ASC');
         }
         else if ($this->sorting=="alphabet-desc")
         {
-            $mealsQuery->whereBetween('name',[$this->min_alphabet,$this->max_alphabet])->orderBy('name', 'DESC')->paginate($this->pagesize);
+            $mealsQuery->whereBetween('name',[$this->min_alphabet,$this->max_alphabet])->orderBy('name', 'DESC');
         }
 
         if ($this->categoryInputs) {
             if ($default_lang == 'en') {
-                $mealsQuery->whereIn('main_cate_id', $this->categoryInputs)->paginate($this->pagesize);
+                $mealsQuery->whereIn('main_cate_id', $this->categoryInputs);
             } else {
                 $main = MainCategory::whereIn('id', $this->categoryInputs)->first();
                 $mealTranslation = MealTranslation::where('main_cate_id', $main->translate_of)->first();
@@ -148,14 +148,14 @@ class RestaurantComponent extends Component
                     $meal = Meal::find($mealTranslation->meal_id);
 
                     if ($meal) {
-                        $mealsQuery->where('category_id', $meal->category_id)->paginate($this->pagesize);
+                        $mealsQuery->where('category_id', $meal->category_id);
                     }
                 }
             }
         }
 
         if ($this->filterProviders) {
-            $mealsQuery->whereIn('provider_id', $this->filterProviders)->paginate($this->pagesize);
+            $mealsQuery->whereIn('provider_id', $this->filterProviders);
         }
 
        if ($this->price_range) {
