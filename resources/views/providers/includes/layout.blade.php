1204b2155b6b5dd5ce225c49d05499d5e3887e42
ayahosnii <96696906+ayahosnii@users.noreply.github.com>
2023-05-05 07:15:04 +0300

    u

--- resources/views/providers/includes/layout.blade.php
+++ resources/views/providers/includes/layout.blade.php
@@ -299,61 +299,7 @@
     </style>
 </head>
 <body class="" style="">z
-<!-- top notificationbar start -->
-<section class="top-2" style="background-image: url(image/top-2.jpg);">
-    <div class="container">
-        <div class="row ">
-            <div class="col">
-                <ul class="top-home">
-                    <li class="top-home-li t-content">
-                        <!-- offer text start -->
-                        <div class="top-content">
-                            <p class="top-slogn"><span class="top-c">free shipping</span> orders from all item</p>
-                        </div>
-                        <!-- offer text end -->
-                    </li>
-                    <li class="top-home-li">
-                        <!-- login start -->
-                        <div class="currency">
-                            <div class="currency-drop">
-                                <ul class="cry">
-                                    <li class="eur-head">
-                                        <span class="eur">account <i class="fa fa-angle-down"></i></span>
-                                        <ul class="all-currency account-details">
-                                            <li><a href="">register / log in</a></li>
-                                            <li><a href="https://spacingtech.com/html/vegist-final/vegist/checkout-2.html">checkout</a></li>
-                                            <li><a href="https://spacingtech.com/html/vegist-final/vegist/wishlist.html">my wishlist</a></li>
-                                            <li><a href="https://spacingtech.com/html/vegist-final/vegist/tracking.html">order history</a></li>
-                                            <li><a href="https://spacingtech.com/html/vegist-final/vegist/cart-2.html">my cart</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- login end -->
-                        <!-- currency start -->
-                        <div class="currency">
-                            <div class="currency-drop">
-                                <ul class="cry">
-                                    <li class="eur-head">
-                                        <span class="eur"><img src="./Vegist - dashboard_files/c-icon1.png" alt="currency icon"> EUR <i class="fa fa-angle-down"></i></span>
-                                        <ul class="all-currency all-drop-currency">
-                                            <li><a href="javascript:void(0)"><img src="./Vegist - dashboard_files/c-icon4.png" alt="currency icon"> AFN</a></li>
-                                            <li><a href="javascript:void(0)"><img src="./Vegist - dashboard_files/c-icon2.png" alt="currency icon"> BDT</a></li>
-                                            <li><a href="javascript:void(0)"><img src="./Vegist - dashboard_files/c-icon3.png" alt="currency icon"> CAD</a></li>
-                                            <li><a href="javascript:void(0)"><img src="./Vegist - dashboard_files/c-icon5.png" alt="currency icon"> GBP</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- currency start -->
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
+
 <!--home page slider start-->
 <section class="home-slider-2">
     <div class="container">
