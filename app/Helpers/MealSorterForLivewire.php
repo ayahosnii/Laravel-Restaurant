4a05a931066d27309a571bcdc628556e421e684b
ayahosnii <96696906+ayahosnii@users.noreply.github.com>
2023-06-14 22:53:09 +0300

    SRP/OCP

--- app/Helpers/MealSorterForLivewire.php
+++ app/Helpers/MealSorterForLivewire.php
@@ -8,22 +8,15 @@ class MealSorterForLivewire
 {
     public static function sortMeals($mealsQuery, array $parameters)
     {
-        dd($parameters);
-        if ($sorting == 'date') {
-           //$this->scopeDate($mealsQuery, array $parameters);
-        } elseif ($sorting == 'date-desc') {
-            $mealsQuery->whereBetween('id', [$minDate, $maxDate])->orderBy('id', 'DESC');
-        } elseif ($sorting == 'price') {
-            $mealsQuery->whereBetween('price', [$minPrice, $maxPrice])->orderBy('price', 'ASC');
-        } elseif ($sorting == 'price-desc') {
-            $mealsQuery->whereBetween('price', [$minPrice, $maxPrice])->orderBy('price', 'DESC');
-        } elseif ($sorting == 'alphabet') {
-            $mealsQuery->whereBetween('name', [$minAlphabet, $maxAlphabet])->orderBy('name', 'ASC');
-        } elseif ($sorting == 'alphabet-desc') {
-            $mealsQuery->whereBetween('name', [$minAlphabet, $maxAlphabet])->orderBy('name', 'DESC');
+        $sorting = $parameters['sorting'];
+
+        $sortingMethod = 'scope' . ucfirst($sorting);
+        if (method_exists(self::class, $sortingMethod)) {
+            self::$sortingMethod($mealsQuery, $parameters);
         }
 
         return $mealsQuery;
+
     }
 
     public function scopeDate($mealsQuery, array $parameters)
@@ -32,15 +25,15 @@ class MealSorterForLivewire
             ->orderBy('id', $parameters['orderDirection'] ?? 'ASC');
     }
 
-    protected static function scopePrice(Builder $query, array $parameters)
+    protected static function scopePrice($mealsQuery, array $parameters)
     {
-        $query->whereBetween('price', [$parameters['min_price'], $parameters['max_price']])
+        $mealsQuery->whereBetween('price', [$parameters['min_price'], $parameters['max_price']])
             ->orderBy('price', $parameters['orderDirection'] ?? 'ASC');
     }
 
-    protected static function scopeAlphabet(Builder $query, array $parameters)
+    protected static function scopeAlphabet($mealsQuery, array $parameters)
     {
-        $query->whereBetween('name', [$parameters['min_alphabet'], $parameters['max_alphabet']])
+        $mealsQuery->whereBetween('name', [$parameters['min_alphabet'], $parameters['max_alphabet']])
             ->orderBy('name', $parameters['orderDirection'] ?? 'ASC');
     }
 }
